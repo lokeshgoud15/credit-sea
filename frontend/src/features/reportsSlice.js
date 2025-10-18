804da5a717a46f75d175b5fd08abdb252f9b08d4
Lokesh Nalamasa <nlokeshgoud7@gmail.com>
2025-10-18 16:07:48 +0530

    vite api url added

--- frontend/src/features/reportsSlice.js
+++ frontend/src/features/reportsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:4000/api";
+const API_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const uploadReport = createAsyncThunk(
   "reports/uploadReport",
